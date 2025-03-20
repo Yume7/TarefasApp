6ac2a97eebdb32e02abfba9b4c0bbf444e661053
Yume7 <yume_owned@hotmail.com>
2025-03-20 12:22:36 -0300

    fix(validation): Corrigido redirecionamento indevido ao cadastrar tarefa com erro, validação do form de cadastro tarefa

---
+++ TarefasApp.UI/Models/TarefaRequestModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace TarefasApp.UI.Models
+{
+    public class TarefaRequestModel
+    {
+        [Required(ErrorMessage = "Por favor, informe o título.")]
+        [MaxLength(100, ErrorMessage = "O título deve ter no máximo {1} caracteres.")]
+        public string Titulo { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Por favor, informe a descrição.")]
+        [MaxLength(300, ErrorMessage = "A descrição deve ter no máximo {1} caracteres.")]
+        public string Descricao { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Por favor, informe a data.")]
+        public DateTime Data { get; set; } = DateTime.Today;
+
+        [Required(ErrorMessage = "Por favor, informe o status.")]
+        public string Status { get; set; }
+    }
+}
